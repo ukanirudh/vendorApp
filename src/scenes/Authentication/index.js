c388eb5e339dacf9cd04c31a2f8a2e0153a74300
Anirudh uk <anirudh.kukkilaya@jifflenow.com>
2018-02-01 15:06:50 +0530

    Anirudh : Changing the layout for vendor/cleint index page

--- src/scenes/Authentication/index.js
+++ src/scenes/Authentication/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
-import { Container, Divider, Grid, Header, List, Segment, Button } from 'semantic-ui-react'
+import { Grid, Header, Button, Segment, Icon, Dimmer } from 'semantic-ui-react'
 import CreateBrowserHistory from '../../commonComponents/CreateBrowserHistory'
 
 /*Imported components*/
-import { AppHeader } from '../../commonComponents'
+import { AppHeader, AppFooter } from '../../commonComponents'
 import './authentication.css'
 
 class Authentication extends Component {
+
+  componentWillMount = () => {
+    this.setState({
+      vendorActionsVisible: false,
+      clientActionsVisible: false
+    })
+  }
+
   vendorLogin = () => {
     CreateBrowserHistory.push({
       pathname: "/login",
@@ -33,95 +41,99 @@ class Authentication extends Component {
     })
   }
 
-  render() {
+  vendorContent = () => {
     return (
       <div>
+        <Header as='h2' inverted>Vendor</Header>
+        <Button color='blue' onClick={this.vendorLogin}>
+          Login in
+        </Button>
+        <Button color='blue' onClick={this.vendorSignup}>
+          Sign Up
+        </Button>
+      </div>
+    )
+  }
+
+  clientContent = () => {
+    return (
+      <div>
+        <Header as='h2' inverted>Client</Header>
+        <Button color='blue' onClick={this.clientLogin}>
+          Login in
+        </Button>
+        <Button color='blue' onClick={this.clientSignup}>
+          Sign Up
+        </Button>
+      </div>
+    )
+  }
+
+  handleFilterState = (currentActionUser, visibleFlag) => {
+    console.log(currentActionUser)
+    switch(currentActionUser) {
+      case 'vendor' :
+        this.setState({ vendorActionsVisible: visibleFlag })
+        break;
+      case 'client' :
+        this.setState({ clientActionsVisible: visibleFlag })
+        break;
+      case 'default' :
+        break;
+    }
+  }
+
+  handleShow = (currentActionUser) => {
+    this.handleFilterState(currentActionUser, true)
+  }
+  handleHide = ( currentActionUser ) => {
+    this.handleFilterState(currentActionUser, false)
+  }
+
+  render() {
+    const { active } = this.state
+    return (
+      <div style={{ padding: '0px 10px' }}>
         <AppHeader />
-        <Container text style={{ marginTop: '7em' }}>
-          <h2> Login As </h2>
-          <Segment.Group horizontal>
-            <Segment>
-              <Button animated='fade' size='massive' color='blue' onClick={this.vendorLogin}>
-                <Button.Content visible>
-                  Vendor
-                </Button.Content>
-                <Button.Content hidden>
-                  Login in
-                </Button.Content>
-              </Button>
-              <Divider horizontal>Or</Divider>
-              <Button color='black' onClick={this.vendorSignup}>
-                Sign Up
-              </Button>
-            </Segment>
-            <Segment>
-              <Button animated='fade' size='massive' color='blue' onClick={this.clientLogin}>
-                <Button.Content visible >
-                  Client
-                </Button.Content>
-                <Button.Content hidden >
-                  Login in
-                </Button.Content>
-              </Button>
-              <Divider horizontal>Or</Divider>
-              <Button color='black' onClick={this.clientSignup}>
-                Sign Up
-              </Button>
-            </Segment>
-          </Segment.Group>
-        </Container>
+        <Segment text style={{ marginTop: '7em' }}>
+          <h2> You are A </h2>
+          <Grid
+            style={{ height: '100%', marginTop: 45 }}
+            verticalAlign='middle'
+          >
+          <Grid.Row  centered columns={3}>
+            <Grid.Column className='user-type-grid'>
+              <Dimmer.Dimmable
+              className='user-type-content'
+              as={Segment}
+              dimmed={this.state.vendorActionsVisible}
+              onMouseEnter={ () => this.handleShow('vendor')}
+              onMouseLeave={ () => this.handleHide('vendor')}>
+              <Dimmer active={this.state.vendorActionsVisible}>
+                {this.vendorContent()}
+              </Dimmer>
+                <Header as='h3' textAlign='center'>Vendor</Header>
+              </Dimmer.Dimmable>
+            </Grid.Column>
 
-        <Segment
-          inverted
-          vertical
-          style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
-        >
-          <Container textAlign='center'>
-            <Grid divided inverted stackable>
-              <Grid.Row>
-                <Grid.Column width={3}>
-                  <Header inverted as='h4' content='Group 1' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as='h4' content='Group 2' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as='h4' content='Group 3' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as='h4' content='Footer Header' />
-                  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
+            <Grid.Column className='user-type-grid'>
+              <Dimmer.Dimmable
+              className='user-type-content'
+              as={Segment}
+              dimmed={this.state.clientActionsVisible}
+              onMouseEnter={ () => this.handleShow('client')}
+              onMouseLeave={ () => this.handleHide('client')}>
+              <Dimmer active={this.state.clientActionsVisible}>
+                {this.clientContent()}
+              </Dimmer>
+                <Header as='h3' textAlign='center'>Client</Header>
+              </Dimmer.Dimmable>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
 
-            <Divider inverted section />
-            <List horizontal inverted divided link>
-              <List.Item as='a' href='#'>Site Map</List.Item>
-              <List.Item as='a' href='#'>Contact Us</List.Item>
-              <List.Item as='a' href='#'>Terms and Conditions</List.Item>
-              <List.Item as='a' href='#'>Privacy Policy</List.Item>
-            </List>
-          </Container>
         </Segment>
+        <AppFooter />
       </div>
     )
   }

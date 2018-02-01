a4fab5a398eb03a29aefa2fd0ec7548cf6e03df4
Anirudh uk <anirudh.kukkilaya@jifflenow.com>
2018-02-01 15:04:14 +0530

    Anirudh : Changing the layout for vendor/cleint index page

--- src/commonComponents/index.js
+++ src/commonComponents/index.js
@@ -1,5 +1,6 @@
 import CreateBrowserHistory from './CreateBrowserHistory';
 import AppHeader from './AppHeader';
+import AppFooter from './AppFooter';
 import Routes from './Routes';
 
-export { CreateBrowserHistory, AppHeader, Routes }
+export { CreateBrowserHistory, AppHeader, AppFooter, Routes }

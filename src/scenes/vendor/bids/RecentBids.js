15b00e9c004e878c7ba0efd546c02242b2fa864a
Anirudh uk <anirudh.kukkilaya@jifflenow.com>
2018-02-02 17:26:39 +0530

    Anirudh : Proposed vendorHome page

---
+++ src/scenes/vendor/bids/RecentBids.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Grid, Card, Image, Button, Segment, Header } from 'semantic-ui-react'
+
+class RecentBids extends Component {
+  render() {
+    return (
+      <Segment>
+        <Header as='h2' textAlign='center'>
+          3 MOST RECENT BIDS
+        </Header>
+        <Grid container centered doubling columns={3}>
+          <Grid.Column>
+            <Card>
+              <Card.Content>
+                <Card.Header>
+                  Steve Sanders
+                </Card.Header>
+                <Card.Meta>
+                  Friends of Elliot
+                </Card.Meta>
+                <Card.Description>
+                  Steve wants to add you to the group <strong>best friends</strong>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='green'>Approve</Button>
+                  <Button basic color='red'>Decline</Button>
+                </div>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+
+          <Grid.Column>
+            <Card>
+              <Card.Content>
+                <Card.Header>
+                  Steve Sanders
+                </Card.Header>
+                <Card.Meta>
+                  Friends of Elliot
+                </Card.Meta>
+                <Card.Description>
+                  Steve wants to add you to the group <strong>best friends</strong>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='green'>Approve</Button>
+                  <Button basic color='red'>Decline</Button>
+                </div>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+
+          <Grid.Column>
+            <Card>
+              <Card.Content>
+                <Card.Header>
+                  Steve Sanders
+                </Card.Header>
+                <Card.Meta>
+                  Friends of Elliot
+                </Card.Meta>
+                <Card.Description>
+                  Steve wants to add you to the group <strong>best friends</strong>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='green'>Approve</Button>
+                  <Button basic color='red'>Decline</Button>
+                </div>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+
+        </Grid>
+      </Segment>
+    );
+  }
+}
+
+export default RecentBids;

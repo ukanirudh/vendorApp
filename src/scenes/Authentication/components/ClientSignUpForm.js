4df527d4b64fb43a8380fb3d35286946302c7282
manojh <hmmanojk@gmail.com>
2018-02-04 15:59:43 +0530

    Adding on submit functionality
    client form submit

--- src/scenes/Authentication/components/ClientSignUpForm.js
+++ src/scenes/Authentication/components/ClientSignUpForm.js
@@ -5,24 +5,28 @@ import { Button, Form, Grid, Header, Message, Segment,Checkbox} from 'semantic-u
 class ClientSignUpForm extends Component {
 
   onClientSignUp = () => {
-      console.log('ClientSignUpForm')
+      console.log('ClientSignUpForm');
+      const { name, email, password, phone } = this.state
+      console.log(name, email, password, phone)
   }
 
-   handleChange() {
+  handleChangeForTerms = () => {
     this.setState({
       checked: !this.state.checked
     })
   } 
-
-  
-  constructor(props) {
-    super(props);
-    this.state = { checked: false };
-    this.handleChange = this.handleChange.bind(this);
+  componentWillMount() {
+    this.setState({
+      checked: false,
+      name: '', email: '',password: '',phone: '', confimPassword:''
+    })
   }
 
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
   render() {
-    console.log(this.props)
+    //console.log(this.props)
+    const { name= '', email= '',password= '',confimPassword='',phone= '' } = this.state
     return (
       <Grid
         columns={3}
@@ -36,34 +40,69 @@ class ClientSignUpForm extends Component {
           </Header>
         </Grid.Row>
         <Grid.Column className='login-form-grid'>
-          <Form size='large' >
+          <Form size='large'
+            onSubmit={this.onClientSignUp}
+           >
             <Segment stacked>
-              <Form.Input
-                fluid
-                icon='mail'
-                iconPosition='left'
-                placeholder='E-mail address'
-              />
-              <Form.Input
+
+            <Form.Input
                 fluid
                 icon='user'
                 iconPosition='left'
                 placeholder='Name'
+                name='name' 
+                value={name} 
+                onChange={this.handleChange}
               />
+              <Form.Input
+                fluid
+                icon='mail'
+                iconPosition='left'
+                name= 'email'
+                value={email}
+                placeholder='E-mail address'
+                onChange={this.handleChange}
+              />
+              
               <Form.Input
                 fluid
                 icon='phone'
                 iconPosition='left'
+                name='phone'
+                value={phone}
                 placeholder='Phone Number'
+                onChange={this.handleChange}
+              />
+              
+              <Form.Input
+                fluid
+                icon='protect'
+                iconPosition='left'
+                value={password}
+                name='password'
+                placeholder='Password'
+                onChange={this.handleChange}
+              />
+
+              <Form.Input
+                fluid
+                icon='protect'
+                iconPosition='left'
+                name ='confimPassword'
+                value={confimPassword}
+                placeholder='Confim Password'
+                onChange={this.handleChange}
               />
               <Form.Field
               checked={ this.state.checked } 
-              onChange={ this.handleChange } 
+              onChange={ this.handleChangeForTerms } 
               control={Checkbox}
               label={<label>I agree to the Terms and Conditions</label>}
               />
 
-              <Button disabled = {! this.state.checked} color='teal' fluid size='large' onClick={this.onClientSignUp}>Sign Up!</Button>
+              
+
+              <Button disabled = {! this.state.checked} color='teal' fluid size='large' onClick={this.onClientSignUp}  >Sign Up!</Button>
             </Segment>
           </Form>
         </Grid.Column>

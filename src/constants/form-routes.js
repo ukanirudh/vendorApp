2badc54a6b24511f91b0f57e5da1d3234f5a0c1a
Anirudh uk <anirudh.kukkilaya@jifflenow.com>
2018-03-06 20:42:08 +0530

    Anirudh : Addin route functionality after tender add

---
+++ src/constants/form-routes.js
@@ -0,0 +1,5 @@
+const formRoutes = {
+  'PostTender': '/client',
+}
+
+export default formRoutes;
--- src/utils/GenericForm.js
+++ src/utils/GenericForm.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import { reduxForm } from 'redux-form';
 import ReduxForm from './ReduxForm'
 import validate from './postTenderValidation'
-
+import formRoutes from '../constants/form-routes'
+import { CreateBrowserHistory } from '../commonComponents'
 
 const Form = (props) => {
   return <ReduxForm formFields={props.formFields} />
@@ -12,6 +13,13 @@ export const EntityForm = ({name, onUpdate, type, fields}) => reduxForm({
   form: name,
   validate,
   onSubmit: onUpdate,
+  onSubmitSuccess: (result, dispatch, props) => {
+    const {form} = props
+    console.log(formRoutes)
+    CreateBrowserHistory.push({
+      pathname: formRoutes[form]
+    })
+  },
   onSubmitFail : (errors, submitError, props) => {console.log(errors, submitError, props)},
   formFields: fields
 })(Form)

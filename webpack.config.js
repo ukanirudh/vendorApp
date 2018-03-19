d626816befceba0538d99a757f85410700b85915
Anirudh uk <anirudh.kukkilaya@jifflenow.com>
2018-03-19 11:56:32 +0530

    Anirudh : SUccessfull webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+// process.env.BABEL_ENV = 'development';
+// process.env.NODE_ENV = 'development';
 var path = require('path');
 var webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
@@ -16,6 +18,55 @@ module.exports = {
             use: {
               loader: "babel-loader"
             }
+          },
+          {
+            test: /\.(css|scss)$/,
+            use: [
+              {
+                loader: 'style-loader'
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1
+                }
+              },
+              // {
+              //   loader: 'sass-loader'
+              // },
+              // {
+              //   loader: 'postcss-loader',
+              //   options: {
+              //     // Necessary for external CSS imports to work
+              //     // https://github.com/facebookincubator/create-react-app/issues/2677
+              //     ident: 'postcss',
+              //     plugins: () => [
+              //       require('postcss-flexbugs-fixes'),
+              //       autoprefixer({
+              //         browsers: [
+              //           '>1%',
+              //           'last 4 versions',
+              //           'Firefox ESR',
+              //           'not ie < 9' // React doesn't support IE8 anyway
+              //         ],
+              //         flexbox: 'no-2009'
+              //       })
+              //     ]
+              //   }
+              // }
+            ]
+          },
+          {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [
+              'file-loader',
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  bypassOnDebug: true,
+                },
+              },
+            ],
           }
         ]
     },

ff6f532f5fb267ce4010c42739f3610dcbe59868
Anirudh uk <anirudh.kukkilaya@jifflenow.com>
2018-03-31 11:20:33 +0530

    Anirudh : Adding responseive wrapper to details page

--- src/scenes/vendor/bids/TenderBidDetailsComp.js
+++ src/scenes/vendor/bids/TenderBidDetailsComp.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react'
-import { Segment, Header, Card, Button, Statistic, Input,
-   Icon, Grid, List, Transition } from 'semantic-ui-react'
+import { Segment, Header, Card, Button, Statistic, Input, Label, Icon, Grid, List } from 'semantic-ui-react'
 import TenderBasicDetailsTemplate from './TenderBasicDetailsTemplate'
-import { CreateBrowserHistory } from '../../../commonComponents'
+import { ResponsiveContainer, CreateBrowserHistory } from '../../../commonComponents'
+
+const AppHeaderProps = {
+  'headerRightActionText': 'Profile',
+  'headerRightAction': () => {
+     CreateBrowserHistory.push({
+      pathname: "/vendor/Profile"
+    })
+  }
+}
 
 class TenderBidDetailsComp extends Component {
 
@@ -26,7 +34,7 @@ class TenderBidDetailsComp extends Component {
      })
     }
     const {tender, position, attemptsRemaining, value} = tender_details
-    this.setState({tenderDetails: tender, otherDetails: {position, attemptsRemaining, value}})
+    this.setState({tenderDetails: tender, otherDetails: {position, attemptsRemaining, value}, bidValue: value})
   }
 
   placeBid = () => {
@@ -40,58 +48,49 @@ class TenderBidDetailsComp extends Component {
     this.setState({bidValue: value})
   }
 
-  handleVisibility = () => this.setState({ visible: !this.state.visible })
-
   render () {
-    const {tenderDetails, otherDetails} = this.state
+    const {tenderDetails, otherDetails, bidValue} = this.state
     const {quantity='', tenderEnds='', sub_category=''} = tenderDetails
     const {position, attemptsRemaining, value} = otherDetails
     const subCategoryName = sub_category ? sub_category.name : ''
     return (
-      <Segment textAlign='center' style={{marginBottom:15}}>
-        <Header as='h2'> Tender Details
-          {position && <Statistic floated='right'>
-            <Statistic.Value> <Icon name='star' /> {position} </Statistic.Value>
-            <Statistic.Label>Rank</Statistic.Label>
-          </Statistic>}
-        </Header>
-        <Grid divided>
-          <Grid.Row>
-            <Grid.Column  width={5}>
-              <Header as='h2'> Details uploaded by Client </Header>
-              <List size={'huge'}>
-                <List.Item> <Icon name='check' color='green' /> Bank Statement  </List.Item>
-                <List.Item> <Icon name='close' color='red' /> New York, NY </List.Item>
-                <List.Item> <Icon name='check' color='green' /> Semantic UI2  </List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={11}>
-              <Header as='h2'> Requirement Details </Header>
-              {TenderBasicDetailsTemplate({material: subCategoryName, quantity, description: 'Description', elapses_in: tenderEnds})}
-              <Transition.Group animation={'fade left'} duration={500}>
-                {!this.state.visible &&
-                  <Grid centered columns={2}>
-                    <Grid.Column>
-                      <Button onClick={this.handleVisibility} style={{marginTop:20}} primary> Place Bid </Button>
-                    </Grid.Column>
-                  </Grid>
-                  }
-              </Transition.Group>
-              <Transition.Group animation={'fade right'} duration={500}>
-                {this.state.visible &&
+      <ResponsiveContainer AppHeaderProps={AppHeaderProps} location={'/vendor'} >
+        <Segment textAlign='center'>
+          <Header as='h2'> Tender Details
+            {position && <Statistic floated='right'>
+              <Statistic.Value> <Icon name='star' /> {position} </Statistic.Value>
+              <Statistic.Label>Rank</Statistic.Label>
+            </Statistic>}
+          </Header>
+          <Grid divided>
+            <Grid.Row>
+              <Grid.Column  width={5}>
+                <Header as='h2'> Details uploaded by Client </Header>
+                <List size={'huge'}>
+                  <List.Item> <Icon name='check' color='green' /> Bank Statement  </List.Item>
+                  <List.Item> <Icon name='close' color='red' /> New York, NY </List.Item>
+                  <List.Item> <Icon name='check' color='green' /> Semantic UI2  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={11}>
+                <Header as='h2'> Requirement Details </Header>
+                {TenderBasicDetailsTemplate({material: subCategoryName, quantity, description: 'Description', elapses_in: tenderEnds})}
+                {attemptsRemaining >= 1 &&
                   <div style={{marginTop:20}}>
-                    <Input name='bid' onChange={this.onBidChange} style={{marginTop: 10}} type='text' placeholder='Search...' action>
-                      <Button onClick={this.placeBid} labelPosition='left' icon='cart' color='teal' content='Place it'/>
-                      <input type='number' />
-                      <Button onClick={this.handleVisibility} icon='remove circle' color='red' />
+                    <Header as='h3'>Your previous Bid value is : Rs {value}</Header>
+                    <Header as='h3'>You can still update your Bid</Header>
+                    <Input name='bid' onChange={this.onBidChange} style={{marginTop: 10}} type='number' placeholder='Search...'>
+                      <Label>$</Label>
+                      <input type='number' value={bidValue}/>
+                      <Button onClick={this.placeBid} labelPosition='left' icon='cart' color='teal' content='Place a new Bid'/>
                     </Input>
                   </div>
                 }
-              </Transition.Group>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </ResponsiveContainer>
     )
   }
 }
--- src/scenes/vendor/bids/TenderDetails.js
+++ src/scenes/vendor/bids/TenderDetails.js
@@ -2,7 +2,16 @@ import React, {Component} from 'react'
 import { Segment, Header, Card, Button, Statistic, Input,
    Icon, Grid, List, Transition } from 'semantic-ui-react'
 import TenderBasicDetailsTemplate from './TenderBasicDetailsTemplate'
-import { CreateBrowserHistory } from '../../../commonComponents'
+import { ResponsiveContainer, CreateBrowserHistory } from '../../../commonComponents'
+
+const AppHeaderProps = {
+  'headerRightActionText': 'Profile',
+  'headerRightAction': () => {
+     CreateBrowserHistory.push({
+      pathname: "/vendor/Profile"
+    })
+  }
+}
 
 class TenderDetails extends Component {
 
@@ -45,45 +54,47 @@ class TenderDetails extends Component {
     const {quantity='', tenderEnds='', sub_category=''} = tenderDetails
     const subCategoryName = sub_category ? sub_category.name : ''
     return (
-      <Segment textAlign='center' style={{marginBottom:15}}>
-        <Header as='h2'> Tender Details </Header>
-        <Grid divided>
-          <Grid.Row>
-            <Grid.Column  width={5}>
-              <Header as='h2'> Details uploaded by Client </Header>
-              <List size={'huge'}>
-                <List.Item> <Icon name='check' color='green' /> Bank Statement  </List.Item>
-                <List.Item> <Icon name='close' color='red' /> New York, NY </List.Item>
-                <List.Item> <Icon name='check' color='green' /> Semantic UI2  </List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={11}>
-              <Header as='h2'> Requirement Details </Header>
-              {TenderBasicDetailsTemplate({material: subCategoryName, quantity, description: 'Description', elapses_in: tenderEnds})}
-              <Transition.Group animation={'fade left'} duration={500}>
-                {!this.state.visible &&
-                  <Grid centered columns={2}>
-                    <Grid.Column>
-                      <Button onClick={this.handleVisibility} style={{marginTop:20}} primary> Place Bid </Button>
-                    </Grid.Column>
-                  </Grid>
+      <ResponsiveContainer AppHeaderProps={AppHeaderProps} location={'/vendor'} >
+        <Segment textAlign='center'>
+          <Header as='h2'> Tender Details </Header>
+          <Grid divided>
+            <Grid.Row>
+              <Grid.Column  width={5}>
+                <Header as='h2'> Details uploaded by Client </Header>
+                <List size={'huge'}>
+                  <List.Item> <Icon name='check' color='green' /> Bank Statement  </List.Item>
+                  <List.Item> <Icon name='close' color='red' /> New York, NY </List.Item>
+                  <List.Item> <Icon name='check' color='green' /> Semantic UI2  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={11}>
+                <Header as='h2'> Requirement Details </Header>
+                {TenderBasicDetailsTemplate({material: subCategoryName, quantity, description: 'Description', elapses_in: tenderEnds})}
+                <Transition.Group animation={'fade left'} duration={500}>
+                  {!this.state.visible &&
+                    <Grid centered columns={2}>
+                      <Grid.Column>
+                        <Button onClick={this.handleVisibility} style={{marginTop:20}} primary> Place Bid </Button>
+                      </Grid.Column>
+                    </Grid>
+                    }
+                </Transition.Group>
+                <Transition.Group animation={'fade right'} duration={500}>
+                  {this.state.visible &&
+                    <div style={{marginTop:20}}>
+                      <Input name='bid' onChange={this.onBidChange} style={{marginTop: 10}} type='text' placeholder='Search...' action>
+                        <Button onClick={this.placeBid} labelPosition='left' icon='cart' color='teal' content='Place it'/>
+                        <input type='number' />
+                        <Button onClick={this.handleVisibility} icon='remove circle' color='red' />
+                      </Input>
+                    </div>
                   }
-              </Transition.Group>
-              <Transition.Group animation={'fade right'} duration={500}>
-                {this.state.visible &&
-                  <div style={{marginTop:20}}>
-                    <Input name='bid' onChange={this.onBidChange} style={{marginTop: 10}} type='text' placeholder='Search...' action>
-                      <Button onClick={this.placeBid} labelPosition='left' icon='cart' color='teal' content='Place it'/>
-                      <input type='number' />
-                      <Button onClick={this.handleVisibility} icon='remove circle' color='red' />
-                    </Input>
-                  </div>
-                }
-              </Transition.Group>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+                </Transition.Group>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </ResponsiveContainer>
     )
   }
 }
